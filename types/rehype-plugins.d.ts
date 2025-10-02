28c338443a0fd97ac6f413a1e74f8ffb02d43b9a
starhqking <1319316@qq.com>
2025-10-02 23:05:16 +0800

    安装了md相关插件

---
+++ types/rehype-plugins.d.ts
@@ -0,0 +1,29 @@
+declare module '@rehypejs/slug' {
+  const rehypeSlug: any;
+  export default rehypeSlug;
+}
+
+declare module '@rehypejs/autolink-headings' {
+  const rehypeAutolinkHeadings: any;
+  export default rehypeAutolinkHeadings;
+}
+
+declare module 'rehype-highlight' {
+  const rehypeHighlight: any;
+  export default rehypeHighlight;
+}
+
+declare module 'rehype-katex' {
+  const rehypeKatex: any;
+  export default rehypeKatex;
+}
+
+declare module 'remark-gfm' {
+  const remarkGfm: any;
+  export default remarkGfm;
+}
+
+declare module 'remark-math' {
+  const remarkMath: any;
+  export default remarkMath;
+}
